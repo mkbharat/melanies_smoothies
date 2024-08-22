b9378db7af433faf46eef2cf806580a0eb5b053b
mkbharat <147367163+mkbharat@users.noreply.github.com>
2024-08-22 17:55:04 +0530

    added pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-#import pandas as pd
+import pandas as pd
 
 
 # Write directly to the app
@@ -19,9 +19,13 @@ st.write("The name on your smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop()
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
 
+#Convert the snowpark Dataframe to a Pandas Dataframe so we can use LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
         "Choose upto 5 ingredients:"
